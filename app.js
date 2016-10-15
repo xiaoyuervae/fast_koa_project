e17fa28672609ece1f6540dd599b3fb33b7aa79e
xiaoyuervae <xiaoyuervae@icloud.com>
2016-10-15 13:35:44 +0800

    add app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+var app = require('koa') ;
+var logger = require('koa-logger') ; 
+var bodyparser = require('koa-bodyparser') ; 
+var staticCache = require('koa-static-cache') ; 
+var errorhandler = require('koa-errorhandler') ; 
+var session = require('koa-generic-session') ; 
+var MongoStore = require('koa-generic-session-mongo') ; 
+var flash = require('koa-flash') ;
+var gzip = require('koa-gzip') ; 
+var scheme = require('koa-scheme') ; 
+var router = require('koa-frouter') ; 
+var routerCache = require('koa-router-cache') ; 
+var render = require('co-ejs') ; 
+var config = require('config-lite') ; 
+
+var merge = require('merge-descriptions') ; 
+var core = require('./lib/core') ; 
+var renderConf = require('config.renderConf') ; 
+merge(renderConf.locals || {} , core , false) ; 
+
+app.keys = [renderConf.locals.$app.name] ; 
+app.use(errorhandler()) ;
+app.use(bodyparser()) ; 
+app.use(staticCache(config.staticCacheConf)) ; 
+app.use(logger()) ; 
+app.use(session({
+	store: new MongoStore(config.mongodb) 
+})) ;
+app.use(flash()) ; 
+app.use(scheme(config.schemeConf)) ; 
+app.use(routerCache(app,config.routerCacheConf)) ; 
+app.use(gzip()) ; 
+app.use(render(app,renderConf)) ; 
+app.use(router(app,config.routerConf)) ;
+
+app.listen(config.port , function() {
+	console.log('server listening on:' , config.port) ;
+})
